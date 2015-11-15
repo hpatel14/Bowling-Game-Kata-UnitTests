0f5c9bd3990d9c0658fd5d64e9ce61ccc871dd10
Harsh Patel <hpatel14@kent.edu>
2015-11-15 13:46:33 -0500

    Bowling Game Kata
    All Unit Tests Passed

---
+++ BowlingGame/BowlingGame.cs
@@ -0,0 +1,49 @@
+﻿//Harsh Patel
+//Bowling Game Kata
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        public void roll(int pins)
+        {
+            Rolls[currentRolls++] = pins;
+        }
+        public int score
+        {
+            get
+            {
+                var score = 0;
+                var indexForRolls = 0;
+                for (var frame = 0; frame < 10; frame++)
+                {
+                    if (Rolls[indexForRolls] == 10)
+                    {
+                        score += Rolls[indexForRolls] + Rolls[indexForRolls + 1] + Rolls[indexForRolls + 2]; // if the frame is strike than execute this code
+                        indexForRolls++;
+                    }
+                    else if (Rolls[indexForRolls] + Rolls[indexForRolls + 1] == 10)
+                    {
+                        score += Rolls[indexForRolls] + Rolls[indexForRolls + 1] + Rolls[indexForRolls + 2]; // if the frame is spare than it executes this code
+                        indexForRolls += 2;
+                    }
+                    else
+                    {
+                        score += Rolls[indexForRolls] + Rolls[indexForRolls + 1]; // otherwise it will execute this part of the code
+                        indexForRolls += 2;
+                    }
+                }
+                return score;
+            }
+        }
+
+        private int[] Rolls = new int[21];
+        private int currentRolls = 0;
+    }
+}
---
+++ BowlingGameTest/Test.cs
@@ -0,0 +1,67 @@
+﻿//Harsh Patel
+//Bowling Game Kata
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BowlingGame;
+
+namespace BowlingGameTest
+{
+    [TestClass]
+    public class Test
+    {
+        private Game game;
+        [TestInitialize]
+        public void Initialize()
+        {
+            game = new Game();
+        }
+
+        [TestMethod]
+        public void RollGutter()
+        {
+            MoreRolls(0, 20);
+            Assert.AreEqual(0, game.score);
+        }
+
+        private void MoreRolls(int pins, int rolls)
+        {
+            for (var i = 0; i < rolls; i++)
+                game.roll(pins);
+        }
+
+        [TestMethod]
+        public void RollOnes()
+        {
+            MoreRolls(1, 20);
+            Assert.AreEqual(20, game.score);
+        }
+
+        [TestMethod]
+        public void RollSpare()
+        {
+            game.roll(5);
+            game.roll(5);
+            game.roll(4);
+            MoreRolls(0, 17);
+            Assert.AreEqual(18, game.score);
+        }
+
+        [TestMethod]
+        public void RollStrike()
+        {
+            game.roll(10);
+            game.roll(3);
+            game.roll(4);
+            MoreRolls(0, 16);
+            Assert.AreEqual(24, game.score);
+        }
+
+        [TestMethod]
+        public void CorrectRole()
+        {
+            MoreRolls(10, 12);
+            Assert.AreEqual(300, game.score);
+        }
+    }
+}
